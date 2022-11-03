c809975f7d73d60884c5f8c0800469c25acdd31a
cleopatra <kelly912718@gmail.com>
2022-11-03 18:45:27 +0800

    Initial Commit

---
+++ TheThing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheThing
+//
+//  Created by 黃郁雯 on 2022/11/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
